847cc2c6b8cffdab8328cbb816fda0cbdc285b9d
Jakson Torres Ruano <Jakson.Torres@tajamar365.com>
2021-04-28 14:21:19 +0200

    inicio de musicos

--- APIConcert/Startup.cs
+++ APIConcert/Startup.cs
@@ -43,6 +43,8 @@ namespace APIConcert
             });
 
 
+            services.AddCors();
+
            
 
         }
@@ -50,6 +52,12 @@ namespace APIConcert
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors(options => options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
